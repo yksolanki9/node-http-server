c75b68f18f25d36de845d8689c8bab59d510e923
Yash Solanki <yashsolanki1709@gmail.com>
2024-03-22 16:13:12 +0530

    Accept and write to connection

--- app/main.js
+++ app/main.js
@@ -9,19 +9,14 @@ const server = net.createServer((socket) => {
     socket.end();
     server.close();
   });
+
+  //Read data from the client
+  socket.on("data", (data) => {
+    console.log("Received data from client!", data.toString());
+
+    //Respond to the client
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  });
 });
-
-const client = net.createConnection({port: 4221}, () => {
-  console.log('Connected to server!');
-})
-
-//Listener for when data is received on the connection
-client.on('data', (data) => {
-  console.log('Received data from server!', data);
-
-  //Write data back to the server
-  client.write('HTTP/1.1 200 OK\r\n\r\n');
-  // client.end();
-})
 
 server.listen(4221, "localhost");
