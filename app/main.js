cb40b36ed817e7b6bd314b254ebd0ae0f26185ed
Yash Solanki <yashsolanki1709@gmail.com>
2024-03-24 23:06:06 +0530

    Send response based on random string

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 const net = require("net");
 
 const HTTP_VERSION = 'HTTP/1.1';
-const CLRF = '\r\n'
+const CR = '\r';
+const LF = '\n';
+const CRLF = CR + LF;
 
 //HTTP Status code and status string map
 const HTTP_STATUS_CODE = {
@@ -11,13 +13,44 @@ const HTTP_STATUS_CODE = {
 
 //Parse and get request method, path and http version from request line
 const parseRequestLine = (data) => {
-  const requestLine = data.toString().split(CLRF)[0];
+  const requestLine = data.toString().split(CRLF)[0];
   return requestLine.split(' ');
 }
 
 //Generate response status line based on status code
 const getResponseStatusLine = (statusCode) => {
-  return HTTP_VERSION + ' ' +  statusCode + ' ' + HTTP_STATUS_CODE[statusCode] + ' ' + CLRF + CLRF
+  return HTTP_VERSION + ' ' +  statusCode + ' ' + HTTP_STATUS_CODE[statusCode] + ' ' + CRLF
+}
+
+//Convert string to JSON
+const getStringFromJson = (json) => {
+  return Object.entries(json).map(([key, value]) => `${key}: ${value}`).join(CRLF) + CRLF
+}
+
+//Generate Representation Headers
+const generateRepresentationHeaders = (body) => {
+  const headerJson = {
+   'Content-Type': 'text/plain',
+   'Content-Length': body?.length ?? 0
+  }
+
+  return getStringFromJson(headerJson)
+}
+
+//Get random string from path
+const getRandomString = (path) => {
+  return path.split('/echo/')[1];
+}
+
+//Generate response string
+const generateResponse = (statusCode, body) => {
+  let res = getResponseStatusLine(statusCode);
+
+  if (body) {
+    res += generateRepresentationHeaders(body);
+    res += LF + body + CRLF;
+  }
+  return res + CRLF;
 }
 
 const server = net.createServer((socket) => {
@@ -29,9 +62,11 @@ const server = net.createServer((socket) => {
   //Read data from the client
   socket.on("data", (data) => {
     const [, path] = parseRequestLine(data);
+    const randomString = getRandomString(path);
+    const res = generateResponse(randomString || path === '/' ? 200 : 404, randomString);
 
     //Respond to the client
-    socket.write(path === '/' ? getResponseStatusLine(200) : getResponseStatusLine(404));
+    socket.write(res);
   });
 });
 
